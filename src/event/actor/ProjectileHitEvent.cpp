750b10cec42145024b7fef5f035d31c4c190a3ea
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-12-24 22:54:39 +0800

    feat:增加抛射物命中事件

---
+++ src/event/actor/ProjectileHitEvent.cpp
@@ -0,0 +1,48 @@
+#include "ProjectileHitEvent.h"
+
+#include "ll/api/event/Emitter.h"
+#include "ll/api/event/EventBus.h"
+#include "ll/api/memory/Hook.h"
+#include "mc/entity/components_json_legacy/ProjectileComponent.h"
+#include "mc/world/actor/Actor.h"
+
+namespace Catalyst {
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    ProjectileHitEventHook,
+    ll::memory::HookPriority::Normal,
+    ProjectileComponent,
+    &ProjectileComponent::onHit,
+    void,
+    ::Actor&           owner,
+    ::HitResult const& hitResult
+) {
+    auto& bus = ll::event::EventBus::getInstance();
+
+    ProjectileHitBeforeEvent beforeEvent(owner, *this, hitResult);
+    bus.publish(beforeEvent);
+    if (beforeEvent.isCancelled()) {
+        return;
+    }
+
+    origin(owner, hitResult);
+
+    ProjectileHitAfterEvent afterEvent(owner, *this, hitResult);
+    bus.publish(afterEvent);
+}
+
+static std::unique_ptr<ll::event::EmitterBase> beforeEmitterFactory();
+class ProjectileHitBeforeEventEmitter : public ll::event::Emitter<beforeEmitterFactory, ProjectileHitBeforeEvent> {
+    ll::memory::HookRegistrar<ProjectileHitEventHook> hook;
+};
+static std::unique_ptr<ll::event::EmitterBase> beforeEmitterFactory() {
+    return std::make_unique<ProjectileHitBeforeEventEmitter>();
+}
+
+static std::unique_ptr<ll::event::EmitterBase> afterEmitterFactory();
+class ProjectileHitAfterEventEmitter : public ll::event::Emitter<afterEmitterFactory, ProjectileHitAfterEvent> {};
+static std::unique_ptr<ll::event::EmitterBase> afterEmitterFactory() {
+    return std::make_unique<ProjectileHitAfterEventEmitter>();
+}
+
+} // namespace Catalyst
---
+++ src/event/actor/ProjectileHitEvent.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ll/api/event/Cancellable.h"
+#include "ll/api/event/entity/ActorEvent.h"
+#include "mc/entity/components_json_legacy/ProjectileComponent.h"
+#include "mc/world/phys/HitResult.h"
+
+#include "Macros.h"
+
+namespace Catalyst {
+
+class CATALYST_API ProjectileHitBeforeEvent final : public ll::event::Cancellable<ll::event::entity::ActorEvent> {
+    ProjectileComponent& mComponent;
+    HitResult const&     mHitResult;
+
+public:
+    constexpr ProjectileHitBeforeEvent(Actor& owner, ProjectileComponent& component, HitResult const& hitResult)
+    : Cancellable(owner),
+      mComponent(component),
+      mHitResult(hitResult) {}
+
+    ProjectileComponent& component() const { return mComponent; }
+    HitResult const&     hitResult() const { return mHitResult; }
+};
+
+class CATALYST_API ProjectileHitAfterEvent final : public ll::event::entity::ActorEvent {
+    ProjectileComponent& mComponent;
+    HitResult const&     mHitResult;
+
+public:
+    constexpr ProjectileHitAfterEvent(Actor& owner, ProjectileComponent& component, HitResult const& hitResult)
+    : ActorEvent(owner),
+      mComponent(component),
+      mHitResult(hitResult) {}
+
+    ProjectileComponent& component() const { return mComponent; }
+    HitResult const&     hitResult() const { return mHitResult; }
+};
+
+} // namespace Catalyst
