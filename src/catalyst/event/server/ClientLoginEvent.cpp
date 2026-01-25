2f0d60d173b1d7904068dfa9a74b6e5c788229bf
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2026-01-26 02:19:47 +0800

    chore:暂时删除代码

--- src/catalyst/event/server/ClientLoginEvent.cpp
+++ src/catalyst/event/server/ClientLoginEvent.cpp
@@ -3,69 +3,13 @@
 #include "ll/api/event/Emitter.h"
 #include "ll/api/event/EventBus.h"
 #include "ll/api/memory/Hook.h"
+#include "mc/certificates/identity/PlayerAuthenticationInfo.h"
 #include "mc/network/ConnectionRequest.h"
 #include "mc/network/NetworkIdentifier.h"
-#include "mc/network/ServerNetworkHandler.h"
 #include "mc/network/ServerConnectionAuthValidator.h"
-#include "mc/certificates/identity/PlayerAuthenticationInfo.h"
+#include "mc/network/ServerNetworkHandler.h"
+
 
 #include "mc/network/packet/LoginPacket.h"
 
 #include <variant>
-/*  
-namespace Catalyst {
-
-LL_TYPE_INSTANCE_HOOK(
-    ClientLoginEventHook,
-    ll::memory::HookPriority::Normal,
-    ServerNetworkHandler,
-    &ServerNetworkHandler::$handle,
-    void,
-    NetworkIdentifier const&     source,
-    std::shared_ptr<LoginPacket> packet
-) {
-    auto& bus = ll::event::EventBus::getInstance();
-
-    ClientLoginBeforeEvent beforeEvent(source, packet);
-    bus.publish(beforeEvent);
-    if (beforeEvent.isCancelled()) {
-        return;
-    }
-
-    origin(source, packet);
-
-    auto* connReq = packet->mConnectionRequest.get();
-    if (connReq) {
-        auto result = mConnectionAuthValidator->validate(*connReq);
-
-        if (auto* authInfo = std::get_if<PlayerAuthenticationInfo>(&result)) {
-            ClientLoginAfterEvent afterEvent(
-                source,
-                packet,
-                authInfo->XboxLiveName,
-                source.getIPAndPort(),
-                authInfo->Xuid,
-                authInfo->Xuid,
-                authInfo->AuthenticatedUuid,
-                connReq->getDeviceId()
-            );
-            bus.publish(afterEvent);
-        }
-    }
-}
-
-static std::unique_ptr<ll::event::EmitterBase> beforeEmitterFactory();
-class ClientLoginBeforeEventEmitter : public ll::event::Emitter<beforeEmitterFactory, ClientLoginBeforeEvent> {
-    ll::memory::HookRegistrar<ClientLoginEventHook> hook;
-};
-static std::unique_ptr<ll::event::EmitterBase> beforeEmitterFactory() {
-    return std::make_unique<ClientLoginBeforeEventEmitter>();
-}
-
-static std::unique_ptr<ll::event::EmitterBase> afterEmitterFactory();
-class ClientLoginAfterEventEmitter : public ll::event::Emitter<afterEmitterFactory, ClientLoginAfterEvent> {};
-static std::unique_ptr<ll::event::EmitterBase> afterEmitterFactory() {
-    return std::make_unique<ClientLoginAfterEventEmitter>();
-}
-
-} // namespace Catalyst
